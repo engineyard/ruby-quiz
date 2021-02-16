ec1e1149055869f03961ecdd5820c0bc06321303
dbroemme <darren.broemmer@gmail.com>
2021-02-15 23:39:29 -0500

    Change logging options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,13 +61,23 @@ Rails.application.configure do
 
   config.require_master_key = true
 
+  #config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
   # Using Semantic Logger for now
   # Similar to Heroku, we need logging to go to STDOUT to get picked up by platform
+  config.log_tags = {
+    request_id: :request_id,
+    ip:         :remote_ip
+  }
+  config.colorize_logging = true
+
+  config.rails_semantic_logger.semantic   = true
+  config.rails_semantic_logger.started    = false
+  config.rails_semantic_logger.processing = false
+  config.rails_semantic_logger.rendered   = false
+
   STDOUT.sync = true
   config.rails_semantic_logger.add_file_appender = false
   config.semantic_logger.add_appender(io: STDOUT, level: config.log_level, formatter: config.rails_semantic_logger.format)
-  #config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-  config.log_tags = [:request_id]
 
-  config.web_console.whitelisted_ips = '10.101.63.36'
+
 end
